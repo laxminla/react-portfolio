a67f38f98a0eeb61fec0b907e89d2cb829c89139
Laxmi Sharma <laxminla@yahoo.com>
2021-11-30 22:00:39 -0800

    added react-router-dom in navigationbar

--- src/components/Navigationbar.js
+++ src/components/Navigationbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../styles/Navigationbar.css';
 import { Navbar, Nav, Container } from "react-bootstrap";
-
+import { Link } from 'react-router-dom';
 function Navigationbar() {
   return (
     <Navbar className="navigation" expand="lg">
@@ -11,10 +11,10 @@ function Navigationbar() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="meAuto" expand="lg">
-              <Nav.Link href="/about">About </Nav.Link>
-              <Nav.Link href="/portfolio">Portfolio</Nav.Link>
-              <Nav.Link href="/contact">Contact</Nav.Link>
-              <Nav.Link href="/resume">Resume</Nav.Link>
+              <Link href="/about">About </Link>
+              <Link href="/portfolio">Portfolio</Link>
+              <Link href="/contact">Contact</Link>
+              <Link href="/resume">Resume</Link>
             </Nav>
           </Navbar.Collapse>
         </div>
